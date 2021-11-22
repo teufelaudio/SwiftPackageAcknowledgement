df49fd1ca1bc16138d9da876d61a890d05d95cff
Malte Bünz <m.buenz@wonderz.eu>
2021-11-22 17:24:05 +0100

    add safe subscript

---
+++ Sources/Helper/Array+Safe.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Array {
+    public subscript(index: Int, default defaultValue: @autoclosure () -> Element) -> Element {
+        guard index >= 0, index < endIndex else {
+            return defaultValue()
+        }
+
+        return self[index]
+    }
+}
